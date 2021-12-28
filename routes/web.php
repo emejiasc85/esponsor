6e78e723fe3a44d301421d9ad3c61ba1a4bdd737
Enrique Mejias <emejiasc85@gmail.com>
2021-12-28 17:49:18 -0600

    add routes to login and register

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
 Route::get('/', function () {
     return view('home');
 });
+
+
